58875ae24109a719c51a47a340d1c3c72590b528
Esther Bawo Tsotso <139592787+estheticallybawo@users.noreply.github.com>
2025-08-26 21:11:21 +0100

    Updated layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,10 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
+  
     <html lang="en">
+    <body>
         {children}
+    </body>
     </html>
-    </>
+  
   );
 }
